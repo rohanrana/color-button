7a88cf214a04008508bc6bfedb31870464a183b1
rohan17391 <rohanrana875@gmaill.com>
2022-10-07 15:16:29 +0530

    Color button TDD test Done

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct intial color", () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+
+  // find a button with role button and text change to blue
+  const colorbutton = screen.getByRole("button", { name: "Change to blue" });
+
+  //intial button color should be red
+  expect(colorbutton).toHaveStyle({ backgroundColor: "red" });
+
+  //Trigger click event
+
+  fireEvent.click(colorbutton);
+
+  //after click button should colored blue
+  expect(colorbutton).toHaveStyle({
+    backgroundColor: "blue",
+  });
+
+  // after click button text should be change to Change to red
+
+  expect(colorbutton.textContent).toBe("Change to red");
 });
+
+test("button has correct intial text", () => {});
